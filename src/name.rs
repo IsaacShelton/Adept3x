bb2e6859e2fd6e2c004e4c5eb7e57f173c934f1e
IsaacShelton <isaac.a.shelton@gmail.com>
2024-09-18 20:42:59 -0500

    Created namespacing data models

---
+++ src/name.rs
@@ -0,0 +1,44 @@
+use std::fmt::Display;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Name {
+    pub namespace: String,
+    pub basename: String,
+}
+
+impl Name {
+    pub fn plain(basename: impl Into<String>) -> Self {
+        Self {
+            namespace: "".into(),
+            basename: basename.into(),
+        }
+    }
+
+    pub fn into_plain(self) -> Option<String> {
+        if self.namespace.is_empty() {
+            Some(self.basename)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_plain_str(&self) -> Option<&str> {
+        if self.namespace.is_empty() {
+            Some(&self.basename)
+        } else {
+            None
+        }
+    }
+}
+
+impl Display for Name {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.namespace, self.basename)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum ResolvedName {
+    Remote(Box<str>),
+    Project(Box<str>),
+}
