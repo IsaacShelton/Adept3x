1507a42665e3b4f017ae6d64997aa3f5890c302a
IsaacShelton <isaac.a.shelton@gmail.com>
2024-12-08 20:50:28 -0600

    Modified 'new' CLI command to error out when given too many arguments

--- src/cli/new/parse.rs
+++ src/cli/new/parse.rs
@@ -2,10 +2,9 @@ use super::NewCommand;
 
 impl NewCommand {
     pub fn parse(mut args: impl Iterator<Item = String>) -> Result<Self, ()> {
-        // Skip over 'new' command keyword
-        args.next().unwrap();
+        args.next().expect("skip over 'new' command keyword");
 
-        let Some(project_name) = args.next() else {
+        let (Some(project_name), None) = (args.next(), args.next()) else {
             println!("adept new <PROJECT_NAME>");
             return Err(());
         };
