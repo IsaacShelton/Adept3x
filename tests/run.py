b0a267d8ea56d1a34013d9a87a1ef4c45cf0077f
IsaacShelton <isaac.a.shelton@gmail.com>
2025-01-02 19:47:19 -0600

    Added simple test cases for implicits

--- tests/run.py
+++ tests/run.py
@@ -11,40 +11,37 @@ error_dir = join(e2e_root_dir, "error")
 
 def run_all_tests():
     executable = sys.argv[1]
-    # test("management_pass", [executable, join(src_dir, "management_pass/main.adept")], compiles)
 
     for entry in sorted(scandir(success_dir), key=lambda ent: ent.name):
         if not entry.is_dir():
             continue
 
-        folder = join(success_dir, entry.name)
-        is_mod = exists(join(folder, "_.adept"))
-
-        cmd = [executable]
-
-        if is_mod:
-            cmd.append(folder)
-        else:
-            cmd.append(join(folder, "main.adept"))
-
-        compiles = lambda _: True
+        cmd = prepare(executable, success_dir, entry)
         test(entry.name, cmd, compiles)
 
     for entry in sorted(scandir(error_dir), key=lambda ent: ent.name):
         if not entry.is_dir():
             continue
 
-        folder = join(error_dir, entry.name)
-        is_mod = exists(join(folder, "_.adept"))
-
-        cmd = [executable]
-
-        if is_mod:
-            cmd.append(folder)
-        else:
-            cmd.append(join(folder, "main.adept"))
-
-        compiles = lambda _: True
+        cmd = prepare(executable, error_dir, entry)
         test(entry.name, cmd, compiles, expected_exitcode="non-zero")
+
+def prepare(executable, dir, entry):
+    folder = join(dir, entry.name)
+    is_mod = exists(join(folder, "_.adept"))
+
+    cmd = [executable]
+
+    if is_mod:
+        cmd.append(folder)
+    else:
+        cmd.append(join(folder, "main.adept"))
+
+    return cmd
+
+
+def compiles(_):
+    return True
+
 
 e2e_framework_run(run_all_tests)
