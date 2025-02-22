cf968907ee202ee10fa346d99677d77a585bece5
IsaacShelton <isaac.a.shelton@gmail.com>
2025-02-21 23:29:25 -0600

    Added test case for simple C binary operators

---
+++ tests/success/c_math/result.h
@@ -0,0 +1,25 @@
+
+#ifndef _C_ADD_RESULT_H_INCLUDED
+#define _C_ADD_RESULT_H_INCLUDED
+
+#define ADD_RESULT 3 + 4
+#define SUBTRACT_RESULT 3 - 4
+#define MULTIPLY_RESULT 3 * 4
+#define DIVIDE_RESULT 21 / 4
+#define MODULUS_RESULT 21 % 4
+#define OR_RESULT 21 || 0
+#define AND_RESULT 21 && 0
+#define BITWISE_OR_RESULT 21 | 2
+#define BITWISE_AND_RESULT 21 & 2
+#define BITWISE_XOR_RESULT 21 ^ 2
+#define EQ_RESULT 21 == 2
+#define NEQ_RESULT 21 != 2
+#define LT_RESULT 21 < 2
+#define GT_RESULT 21 > 2
+#define LTE_RESULT 21 <= 2
+#define GTE_RESULT 21 >= 2
+#define LSHIFT_RESULT 21 << 2
+#define RSHIFT_RESULT 21 >> 2
+
+#endif // _C_ADD_RESULT_H_INCLUDED
+
