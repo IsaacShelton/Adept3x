2bfb68d146e5eac572e8e8043a161e9a327eeee9
IsaacShelton <isaac.a.shelton@gmail.com>
2025-05-11 23:44:35 -0500

    Refactored and cleaned up job request system

--- src/components/job/src/execution/diverge.rs
+++ src/components/job/src/execution/diverge.rs
@@ -4,12 +4,6 @@ use crate::{Executor, Progress, TaskRef, prereqs::Prereqs};
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Diverge {}
 
-impl Diverge {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
 impl<'env> Execute<'env> for Diverge {
     fn execute(self, executor: &Executor<'env>) -> Progress<'env> {
         let cyclic = executor.request(Diverge {});
