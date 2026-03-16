dd65c25ab42bb5af6e05567792ed72a36566385b
IsaacShelton <isaac.a.shelton@gmail.com>
2026-03-15 22:06:48 -0500

    Continued working on language kernel

--- src/kernel/src/lib.rs
+++ src/kernel/src/lib.rs
@@ -1,6 +1,18 @@
-use std::sync::Arc;
-use syntax_tree::SyntaxNode;
+#[cfg(feature = "kernel")]
+mod v1;
 
-pub fn debug_eval(_: &Arc<SyntaxNode>) -> String {
-    todo!("for testing only")
+#[cfg(feature = "kernel")]
+pub use v1::*;
+
+#[cfg(not(feature = "kernel"))]
+mod non_implementation {
+    use std::sync::Arc;
+    use syntax_tree::SyntaxNode;
+
+    pub fn debug_eval(_: &Arc<SyntaxNode>) -> String {
+        todo!("for testing only")
+    }
 }
+
+#[cfg(not(feature = "kernel"))]
+pub use non_implementation::*;
